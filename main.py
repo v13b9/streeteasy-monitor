efb3c61e9738aad7f311695de1de3a47fa4b4f30
Joe Schermer <joeschermer@gmail.com>
2024-05-30 00:27:16 +0000

    Change name to main.py. Set up cron job. Use datetime in logging

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import pytz
 import random
 import re
 import requests
@@ -176,8 +178,17 @@ def wait():
     time.sleep(random.randint(0, 3))
 
 
+def get_datetime():
+    tz = pytz.timezone('America/New_York')
+    now = datetime.now(tz)
+    time_now = now.strftime('%l:%M %p')
+    date_now = now.strftime('%B %e, %Y')
+    return date_now, time_now
+
+
 def try_post(url, json_data, func_name):
-    print(f'\nTrying POST {url} - {func_name}...')
+    date_now, time_now = get_datetime()
+    print(f'\n[{date_now} - {time_now}] Trying POST {url} - {func_name}...')
     wait()
     r = s.post(url, json=json_data)
     print(f'Status code: {r.status_code} {r.reason}')
@@ -185,7 +196,8 @@ def try_post(url, json_data, func_name):
 
 
 def try_get(url, func_name):
-    print(f'Trying GET {url} - {func_name}...')
+    date_now, time_now = get_datetime()
+    print(f'[{date_now} - {time_now}] Trying GET {url} - {func_name}...')
     wait()
     r = s.get(url)
     print(f'Status code: {r.status_code} {r.reason}')
