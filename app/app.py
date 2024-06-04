67c2f9339730f61db1941907b4645c86471b190b
Joe Schermer <joeschermer@gmail.com>
2024-06-04 11:05:29 -0400

    Refactor flask app creation

--- app/app.py
+++ app/app.py
@@ -24,25 +24,39 @@ def format_datetime(created_at):
     return f'{date_formatted} {time_formatted}'
 
 
-# set configuration values
-class Config:
-    SCHEDULER_API_ENABLED = True
-    SCHEDULER_JOB_DEFAULTS = {'misfire_grace_time': None}
-
-
 app = Flask(__name__)
-app.config.from_object(Config())
-app.jinja_env.filters = {
-    'usd': usd,
-    'format_datetime': format_datetime,
-}
 
+def create_app():
+    # set configuration values
+    class Config:
+        SCHEDULER_API_ENABLED = True
+        SCHEDULER_JOB_DEFAULTS = {'misfire_grace_time': None}
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
+    app.config.from_object(Config())
+    app.jinja_env.filters = {
+        'usd': usd,
+        'format_datetime': format_datetime,
+    }
 
+    # Configure session to use filesystem (instead of signed cookies)
+    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Session(app)
+
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.remove_all_jobs()
+
+    scheduler.add_job(
+        id='main',
+        func=main,
+        trigger='interval',
+        seconds=random.randint(12, 18)
+    )
+
+    scheduler.start()
+
+    return app
 
 @app.after_request
 def after_request(response):
@@ -60,17 +74,5 @@ def index():
 
 
 if __name__ == '__main__':
-    scheduler = APScheduler()
-    scheduler.init_app(app)
-    scheduler.remove_all_jobs()
-
-    scheduler.add_job(
-        id='main',
-        func=main,
-        trigger='interval',
-        seconds=random.randint(360, 540)
-    )
-
-    scheduler.start()
-
+    app = create_app()
     app.run()
